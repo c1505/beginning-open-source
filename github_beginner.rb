915eef14fe7d3d3cc892f3c6828a011282ab60c1
Corey Morris <coreym247@gmail.com>
2016-01-10 00:08:16 -0500

    refactoring done

--- github_beginner.rb
+++ github_beginner.rb
@@ -1,18 +1,15 @@
 require 'json'
 require 'httparty'
 require 'pry'
-class GithubBeginner 
-  attr_accessor :result
-  def intitialize
-        @result = "ten"
-  end
 
-  def self.github_commits(user)
+class GithubBeginner 
+  
+  def self.get_issues
       #title
       #body
       #"html_url"
-      
 
+      #repo name
       response = HTTParty.get("https://api.github.com/search/issues?q=label:beginner+language:ruby+state:open&sort=created&order=asc")
       events = JSON.parse(response.body)
       events = events["items"]
@@ -22,20 +19,18 @@ class GithubBeginner
       events.each do |f|
         hash_of_issue = {}
         hash_of_issue[:title] = f["title"]
-        hash_of_issue[:body] = f["body"]
+        # hash_of_issue[:body] = f["body"]  #uncomment
+        #above commented out because the large amount of output makes it difficult 
+        #to see what is going on.
         hash_of_issue[:html_url] = f["html_url"]
         array_of_issues << hash_of_issue
       end
 
-      binding.pry
-
-       #there are only 30 titles and i'm not sure why
+      # binding.pry
+      puts array_of_issues
+       #there are pagination options.  
+       # right now it is only giving me 30 per page.  can go up to 100
     end  
-    
-
-    def self.test_method
-        "string"
-    end
 end
 
-GithubBeginner.github_commits('c1505')
+GithubBeginner.get_issues
